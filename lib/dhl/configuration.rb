1e33375a0df805f270f9ad54ca48c6d655e58cd5
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-08-22 15:04:02 +0200

    add production endpoints

--- lib/dhl/configuration.rb
+++ lib/dhl/configuration.rb
@@ -6,6 +6,7 @@ module Dhl
       @username = ENV['DHL_USERNAME']
       @password = ENV['DHL_PASSWORD']
       @account = ENV['DHL_ACCOUNT']
+      @environment = ENV['DHL_ENVIRONMENT'] || :test
     end
 
   end
