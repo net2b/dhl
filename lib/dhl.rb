3950a9e29a943d02e154452ec680db57a20c343b
Claudio Poli <masterkain@gmail.com>
2014-11-03 12:53:50 +0100

    reformat dhl.rb

--- lib/dhl.rb
+++ lib/dhl.rb
@@ -1,47 +1,45 @@
 require 'savon'
 
-require "dhl/client"
-require "dhl/configuration"
-require "dhl/contact"
-require "dhl/packages"
-require "dhl/package"
-require "dhl/shipment"
-require "dhl/shipment_request"
-require "dhl/tracking"
-require "dhl/error"
+require 'dhl/client'
+require 'dhl/configuration'
+require 'dhl/contact'
+require 'dhl/packages'
+require 'dhl/package'
+require 'dhl/shipment'
+require 'dhl/shipment_request'
+require 'dhl/tracking'
+require 'dhl/error'
 
-require "dhl/version"
+require 'dhl/version'
 
 module Dhl
-
   def self.setup
-    yield self.config
+    yield config
   end
 
   def self.config
     @config ||= Configuration.new
   end
 
-  def self.client(options={})
+  def self.client(options = {})
     @client ||= Client.new(options)
   end
-
 end
 
 class Hash
   def deep_reject(&blk)
-    self.dup.deep_reject!(&blk)
+    dup.deep_reject!(&blk)
   end
 
   def deep_reject!(&blk)
-    self.each do |k, v|
-      v.deep_reject!(&blk)  if v.is_a?(Hash)
-      self.delete(k)  if blk.call(k, v)
+    each do |k, v|
+      v.deep_reject!(&blk) if v.is_a?(Hash)
+      delete(k) if blk.call(k, v)
     end
   end
 
   def remove_empty
-    deep_reject do |key, value|
+    deep_reject do |_key, value|
       value.nil? || value == ''
     end
   end
